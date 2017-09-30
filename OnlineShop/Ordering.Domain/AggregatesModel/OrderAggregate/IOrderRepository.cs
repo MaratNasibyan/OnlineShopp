8db3dcfe35106f593b1a1e760509fc9860048d38
Marat Nasibyan ashoti <marat_17.5@live.com>
2017-09-30 22:47:37 +0400

    Add Order Aggregate Model

---
+++ OnlineShop/Ordering.Domain/AggregatesModel/OrderAggregate/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ordering.Domain.SeedWork;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    //This is just the RepositoryContracts or Interface defined at the Domain Layer
+    //as requisite for the Order Aggregate
+
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Order Add(Order order);
+
+        void Update(Order order);
+
+        Task<Order> GetAsync(int orderId);
+    }
+}
