ed410b6893ecaaf39260f9ee32794693280c1bc7
Marat Nasibyan ashoti <marat_17.5@live.com>
2017-10-15 19:09:31 +0400

    Add Commands

---
+++ OnlineShop/Ordering.API/Application/Commands/CancelOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Ordering.Domain.AggregatesModel.OrderAggregate;
+using Ordering.Infrastructure.Idempotency;
+
+namespace Ordering.API.Application.Commands
+{
+    public class CancelOrderCommandIdentifiedHandler : IdentifierCommandHandler<CancelOrderCommand, bool>
+    {
+        public CancelOrderCommandIdentifiedHandler(IMediator mediator, IRequestManager requestManager) : base(mediator, requestManager)
+        {
+        }
+
+        protected override bool CreateResultForDuplicateRequest()
+        {
+            return true;                // Ignore duplicate requests for processing order.
+        }
+    }
+
+    public class CancelOrderCommandHandler : IAsyncRequestHandler<CancelOrderCommand, bool>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public CancelOrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        /// <summary>
+        /// Handler which processes the command when
+        /// customer executes cancel order from app
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public async Task<bool> Handle(CancelOrderCommand command)
+        {
+            var orderToUpdate = await _orderRepository.GetAsync(command.OrderNumber);
+            orderToUpdate.SetCancelledStatus();
+            return await _orderRepository.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
