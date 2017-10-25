3cf22667794ecba68ba698057beefe0447379fe0
Marat Nasibyan ashoti <marat_17.5@live.com>
2017-10-25 23:03:32 +0400

    Add CommandValidation

---
+++ OnlineShop/Ordering.API/Application/Validations/CancelOrderCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ordering.API.Application.Commands;
+using FluentValidation;
+
+namespace Ordering.API.Application.Validations
+{
+    public class CancelOrderCommandValidator : AbstractValidator<CancelOrderCommand>
+    {
+        public CancelOrderCommandValidator()
+        {
+            RuleFor(order => order.OrderNumber).NotEmpty().WithMessage("No orderId found");
+        }
+    }
+}
---
+++ OnlineShop/Ordering.API/Application/Validations/IdentifierCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Ordering.API.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Application.Validations
+{
+    public class IdentifierCommandValidator : AbstractValidator<IdentifiedCommand<CreateOrderCommand, bool>>
+    {
+        public IdentifierCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty();
+        }
+    }
+}
