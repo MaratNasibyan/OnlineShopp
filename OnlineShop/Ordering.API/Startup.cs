d5265ae33dac891fdecc6547cb103c8f44810336
MaratNasibyan <nasibyan60@mail.ru>
2018-11-29 15:39:01 +0400

    Test1

--- OnlineShop/Ordering.API/Startup.cs
+++ OnlineShop/Ordering.API/Startup.cs
@@ -25,7 +25,7 @@ namespace Ordering.API
         {
             services.AddMvc();
         }
-
+        //Test1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
