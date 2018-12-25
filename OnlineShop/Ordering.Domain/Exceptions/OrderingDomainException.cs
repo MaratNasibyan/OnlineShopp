6509f84fea43d695214b163aee866e2ae0144385
Marat <marat_17.5@mail.ru>
2018-12-25 16:50:58 +0400

    GitKrakrn commit

--- OnlineShop/Ordering.Domain/Exceptions/OrderingDomainException.cs
+++ OnlineShop/Ordering.Domain/Exceptions/OrderingDomainException.cs
@@ -7,7 +7,7 @@ namespace Ordering.Domain.Exceptions
     /// <summary>
     /// Exception type for domain exceptions
     /// </summary>
-
+//5656
     public class OrderingDomainException : Exception
     {
         public OrderingDomainException()
